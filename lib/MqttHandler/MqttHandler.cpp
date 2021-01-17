488f36861408741b7822f811c40eceafb1e37afa
Thorsten Ludewig <th@l9g.de>
2021-01-17 15:28:31 +0100

    implement MQTT funtionality

--- lib/MqttHandler/MqttHandler.cpp
+++ lib/MqttHandler/MqttHandler.cpp
@@ -3,6 +3,9 @@
 #include <PubSubClient.h>
 #include <WiFiClient.h>
 #include <WifiHandler.hpp>
+#include <WebPages.h>
+#include <Util.hpp>
+#include <TuringPiHandler.hpp>
 #include "MqttHandler.hpp"
 
 MqttHandler mqttHandler;
@@ -13,19 +16,42 @@ static long lastReconnectAttempt = 0;
 
 static void callback(char *topic, byte *payload, unsigned int length)
 {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
+  // set_slot 1 power_on 0
+  char command[32];
+  char subcommand[32];
+  int slot = -1;
+  int power_on = -1;
 
-  for (unsigned int i = 0; i < length; i++)
+  unsigned int i;
+  for (i = 0; i < length; i++)
   {
-    Serial.print((char)payload[i]);
+    buffer2[i] = (char)payload[i];
   }
-
-  Serial.println();
+  buffer2[i] = 0;
+  buffer2[31] = 0; // truncate
+  logMessage("MQTT: message arrived [%s] = %s", topic, buffer2);
+  sscanf( buffer2, "%s %d %s %d", command, &slot, subcommand, &power_on );
+  if ( strcmp( "set_slot", command ) == 0 
+    && strcmp( "power_on", subcommand ) == 0
+    && slot >= 1 && slot <= 7
+    && power_on >= 0 && power_on <= 1
+  )
+  {
+    logMessage("MQTT: turning slot %d to power %s.", slot, (power_on==1)?"on":"off");
+    turingPiHandler.setPower( slot, power_on == 1 );
+  }
+  else
+  {
+    logMessage("MQTT(ERROR): unknown command");
+  }
+  
 }
 
-MqttHandler::MqttHandler() { initialized = false; }
+MqttHandler::MqttHandler()
+{
+  initialized = false;
+  lastPublishTimestamp = 0l;
+}
 
 bool MqttHandler::reconnect()
 {
@@ -51,7 +77,7 @@ void MqttHandler::setup()
   initialized = true;
 }
 
-void MqttHandler::handle( unsigned long now)
+void MqttHandler::handle(unsigned long now)
 {
   if (appcfg.mqtt_enabled && wifiHandler.isReady())
   {
@@ -62,7 +88,7 @@ void MqttHandler::handle( unsigned long now)
 
     if (!client.connected())
     {
-      long now = millis();
+      now = millis();
 
       if (now - lastReconnectAttempt > 5000)
       {
@@ -73,24 +99,32 @@ void MqttHandler::handle( unsigned long now)
     else
     {
       client.loop();
+
+      if (appcfg.mqtt_sending_interval > 0 
+         && (now - lastPublishTimestamp) >= (appcfg.mqtt_sending_interval * 1000))
+      {
+        buildJsonMessage();
+        sendValue(appcfg.mqtt_outtopic, buffer);
+        lastPublishTimestamp = now;
+      }
     }
   }
 }
 
-void MqttHandler::sendValue( const char* topic, const char *value )
+void MqttHandler::sendValue(const char *topic, const char *value)
 {
-  if( appcfg.mqtt_enabled && wifiHandler.isReady() && client.connected())
+  if (appcfg.mqtt_enabled && wifiHandler.isReady() && client.connected())
   {
-    if ( topic != NULL && value != NULL && strlen(topic) > 0 && topic[0] != '-' )
+    if (topic != NULL && value != NULL && strlen(topic) > 0 && topic[0] != '-')
     {
-      client.publish( topic, value );
+      client.publish(topic, value);
     }
   }
 }
 
-void MqttHandler::sendValue( const char* topic, const float value )
+void MqttHandler::sendValue(const char *topic, const float value)
 {
   char buffer[32];
-  sprintf( buffer, "%0.2f", value );
-  sendValue( topic, buffer );
+  sprintf(buffer, "%0.2f", value);
+  sendValue(topic, buffer);
 }
